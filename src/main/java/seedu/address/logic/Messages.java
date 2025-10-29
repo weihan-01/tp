4f15aaf2c4ea44c7b640f78de57752f4c742110d
yymeiting <yymeiting@gmail.com>
2025-10-29 17:16:41 +0800

    Update SeniorBuilder, CaregiverBuilder, TypicalPersons, Messages and DeleteCommandTest

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -20,6 +20,8 @@ public class Messages {
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
                 "Multiple values specified for the following single-valued field(s): ";
+    public static final String MESSAGE_INVALID_SENIOR_INDEX = "No such senior index exists.";
+    public static final String MESSAGE_INVALID_CAREGIVER_INDEX = "No such caregiver index exists.";
     public static final String MESSAGE_NO_SENIORS_PROMPT = "No seniors in AddressBook. Use add-snr to add a senior!";
     public static final String MESSAGE_NO_CAREGIVERS_PROMPT =
             "No caregivers in AddressBook. Use add-cgr command to add a caregiver!";
