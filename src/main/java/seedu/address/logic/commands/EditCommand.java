e4d3ef5f23c2e6bb81cd6fc8a0989ec8048ca037
weihan-01 <weihan8500@gmail.com>
2025-10-29 19:12:54 +0800

    edits to edit command to call the 'persons' internal edit function instead

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -1,237 +1,113 @@
-//package seedu.address.logic.commands;
-//
-//import static java.util.Objects.requireNonNull;
-//import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
-//import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-//import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-//import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
-//import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
-//
-//import java.util.Collections;
-//import java.util.HashSet;
-//import java.util.List;
-//import java.util.Objects;
-//import java.util.Optional;
-//import java.util.Set;
-//
-//import seedu.address.commons.core.index.Index;
-//import seedu.address.commons.util.CollectionUtil;
-//import seedu.address.commons.util.ToStringBuilder;
-//import seedu.address.logic.Messages;
-//import seedu.address.logic.commands.exceptions.CommandException;
-//import seedu.address.model.Model;
-//import seedu.address.model.person.Address;
-//import seedu.address.model.person.Name;
-//import seedu.address.model.person.Note;
-//import seedu.address.model.person.Person;
-//import seedu.address.model.person.Phone;
-//import seedu.address.model.tag.Tag;
-//
-///**
-// * Edits the details of an existing person in the address book.
-// */
-//public class EditCommand extends Command {
-//
-//    public static final String COMMAND_WORD = "edit";
-//
-//    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the person identified "
-//            + "by the index number used in the displayed person list. "
-//            + "Existing values will be overwritten by the input values.\n"
-//            + "Parameters: INDEX (must be a positive integer) "
-//            + "[" + PREFIX_NAME + "NAME] "
-//            + "[" + PREFIX_PHONE + "PHONE] "
-//            + "[" + PREFIX_ADDRESS + "ADDRESS] "
-//            + "[" + PREFIX_TAG + "TAG]...\n"
-//            + "Example: " + COMMAND_WORD + " 1 "
-//            + PREFIX_PHONE + "91234567";
-//
-//    public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited Person: %1$s";
-//    public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
-//    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book.";
-//
-//    private final Index index;
-//    private final EditPersonDescriptor editPersonDescriptor;
-//
-//    /**
-//     * @param index of the person in the filtered person list to edit
-//     * @param editPersonDescriptor details to edit the person with
-//     */
-//    public EditCommand(Index index, EditPersonDescriptor editPersonDescriptor) {
-//        requireNonNull(index);
-//        requireNonNull(editPersonDescriptor);
-//
-//        this.index = index;
-//        this.editPersonDescriptor = new EditPersonDescriptor(editPersonDescriptor);
-//    }
-//
-//    @Override
-//    public CommandResult execute(Model model) throws CommandException {
-//        requireNonNull(model);
-//        List<Person> lastShownList = model.getFilteredPersonList();
-//
-//        if (index.getZeroBased() >= lastShownList.size()) {
-//            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
-//        }
-//
-//        Person personToEdit = lastShownList.get(index.getZeroBased());
-//        Person editedPerson = createEditedPerson(personToEdit, editPersonDescriptor);
-//
-//        if (!personToEdit.isSamePerson(editedPerson) && model.hasPerson(editedPerson)) {
-//            throw new CommandException(MESSAGE_DUPLICATE_PERSON);
-//        }
-//
-//        model.setPerson(personToEdit, editedPerson);
-//        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-//        return new CommandResult(String.format(MESSAGE_EDIT_PERSON_SUCCESS, Messages.format(editedPerson)));
-//    }
-//
-//    /**
-//     * Creates and returns a {@code Person} with the details of {@code personToEdit}
-//     * edited with {@code editPersonDescriptor}.
-//     */
-//    private static Person createEditedPerson(Person personToEdit, EditPersonDescriptor editPersonDescriptor) {
-//        assert personToEdit != null;
-//
-//        Name updatedName = editPersonDescriptor.getName().orElse(personToEdit.getName());
-//        Phone updatedPhone = editPersonDescriptor.getPhone().orElse(personToEdit.getPhone());
-//        Address updatedAddress = editPersonDescriptor.getAddress().orElse(personToEdit.getAddress());
-//        // Set<Tag> updatedTags = editPersonDescriptor.getTags().orElse(personToEdit.getTags());
-//        Note updatedNote = editPersonDescriptor.getNote().orElse(personToEdit.getNote());
-//
-//        return new Person(updatedName, updatedPhone, updatedAddress, updatedNote);
-//    }
-//
-//    @Override
-//    public boolean equals(Object other) {
-//        if (other == this) {
-//            return true;
-//        }
-//
-//        // instanceof handles nulls
-//        if (!(other instanceof EditCommand)) {
-//            return false;
-//        }
-//
-//        EditCommand otherEditCommand = (EditCommand) other;
-//        return index.equals(otherEditCommand.index)
-//                && editPersonDescriptor.equals(otherEditCommand.editPersonDescriptor);
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return new ToStringBuilder(this)
-//                .add("index", index)
-//                .add("editPersonDescriptor", editPersonDescriptor)
-//                .toString();
-//    }
-//
-//    /**
-//     * Stores the details to edit the person with. Each non-empty field value will replace the
-//     * corresponding field value of the person.
-//     */
-//    public static class EditPersonDescriptor {
-//        private Name name;
-//        private Phone phone;
-//        private Address address;
-//        private Set<Tag> tags;
-//        private Note note;
-//
-//        public EditPersonDescriptor() {}
-//
-//        /**
-//         * Copy constructor.
-//         * A defensive copy of {@code tags} is used internally.
-//         */
-//        public EditPersonDescriptor(EditPersonDescriptor toCopy) {
-//            setName(toCopy.name);
-//            setPhone(toCopy.phone);
-//            setAddress(toCopy.address);
-//            setTags(toCopy.tags);
-//            setNote(toCopy.note);
-//        }
-//
-//        /**
-//         * Returns true if at least one field is edited.
-//         */
-//        public boolean isAnyFieldEdited() {
-//            return CollectionUtil.isAnyNonNull(name, phone, address, tags);
-//        }
-//
-//        public void setName(Name name) {
-//            this.name = name;
-//        }
-//
-//        public Optional<Name> getName() {
-//            return Optional.ofNullable(name);
-//        }
-//
-//        public void setPhone(Phone phone) {
-//            this.phone = phone;
-//        }
-//
-//        public Optional<Phone> getPhone() {
-//            return Optional.ofNullable(phone);
-//        }
-//
-//        public void setAddress(Address address) {
-//            this.address = address;
-//        }
-//
-//        public Optional<Address> getAddress() {
-//            return Optional.ofNullable(address);
-//        }
-//
-//        /**
-//         * Sets {@code tags} to this object's {@code tags}.
-//         * A defensive copy of {@code tags} is used internally.
-//         */
-//        public void setTags(Set<Tag> tags) {
-//            this.tags = (tags != null) ? new HashSet<>(tags) : null;
-//        }
-//
-//        /**
-//         * Returns an unmodifiable tag set, which throws {@code UnsupportedOperationException}
-//         * if modification is attempted.
-//         * Returns {@code Optional#empty()} if {@code tags} is null.
-//         */
-//        public Optional<Set<Tag>> getTags() {
-//            return (tags != null) ? Optional.of(Collections.unmodifiableSet(tags)) : Optional.empty();
-//        }
-//
-//        public void setNote(Note note) {
-//            this.note = note;
-//        }
-//
-//        public Optional<Note> getNote() {
-//            return Optional.ofNullable(note);
-//        }
-//
-//        @Override
-//        public boolean equals(Object other) {
-//            if (other == this) {
-//                return true;
-//            }
-//
-//            // instanceof handles nulls
-//            if (!(other instanceof EditPersonDescriptor)) {
-//                return false;
-//            }
-//
-//            EditPersonDescriptor otherEditPersonDescriptor = (EditPersonDescriptor) other;
-//            return Objects.equals(name, otherEditPersonDescriptor.name)
-//                    && Objects.equals(phone, otherEditPersonDescriptor.phone)
-//                    && Objects.equals(address, otherEditPersonDescriptor.address)
-//                    && Objects.equals(tags, otherEditPersonDescriptor.tags);
-//        }
-//
-//        @Override
-//        public String toString() {
-//            return new ToStringBuilder(this)
-//                    .add("name", name)
-//                    .add("phone", phone)
-//                    .add("address", address)
-//                    .add("tags", tags)
-//                    .toString();
-//        }
-//    }
-//}
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Caregiver;
+import seedu.address.model.person.Senior;
+
+/**
+ * Edits the details of an existing senior or caregiver in the address book.
+ */
+public class EditCommand extends Command {
+
+    public static final String COMMAND_WORD = "edit";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Edits the details of the person identified by the index number "
+            + "used in the displayed person list.\n"
+            + "Example: " + COMMAND_WORD + " 1 n/John Tan p/91234567";
+
+    public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited %1$s: %2$s";
+    public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
+    public static final String MESSAGE_INVALID_INDEX = "The person index provided is invalid.";
+
+    private final int index; // using int instead of Index class
+    private final EditPersonDescriptor editPersonDescriptor;
+    private final boolean isSenior; // true if editing a Senior, false if editing a Caregiver
+
+    /**
+     * Creates an EditCommand to edit the person at the specified index.
+     *
+     * @param index index of the person in the filtered list to edit
+     * @param editPersonDescriptor details to edit the person with
+     * @param isSenior whether the person is a Senior (true) or a Caregiver (false)
+     */
+    public EditCommand(int index, EditPersonDescriptor editPersonDescriptor, boolean isSenior) {
+        requireNonNull(editPersonDescriptor);
+        this.index = index;
+        this.editPersonDescriptor = editPersonDescriptor;
+        this.isSenior = isSenior;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+        if (isSenior) {
+            // --- Edit Senior ---
+            List<Senior> lastShownList = model.getFilteredSeniorList();
+
+            if (index < 0 || index >= lastShownList.size()) {
+                throw new CommandException(MESSAGE_INVALID_INDEX);
+            }
+
+            Senior seniorToEdit = lastShownList.get(index);
+
+            if (!editPersonDescriptor.isAnyFieldEdited()) {
+                throw new CommandException(MESSAGE_NOT_EDITED);
+            }
+
+            Senior editedSenior = seniorToEdit.edit(editPersonDescriptor);
+
+            if (!seniorToEdit.isSamePerson(editedSenior) && model.hasPerson(editedSenior)) {
+                throw new CommandException("This senior already exists in the address book.");
+            }
+
+            model.setSenior(seniorToEdit, editedSenior);
+            model.updateFilteredSeniorList(Model.PREDICATE_SHOW_ALL_PERSONS);
+
+            return new CommandResult(
+                    String.format(MESSAGE_EDIT_PERSON_SUCCESS, "Senior", Messages.format(editedSenior)));
+
+        } else {
+            // --- Edit Caregiver ---
+            List<Caregiver> lastShownList = model.getFilteredCaregiverList();
+
+            if (index < 0 || index >= lastShownList.size()) {
+                throw new CommandException(MESSAGE_INVALID_INDEX);
+            }
+
+            Caregiver caregiverToEdit = lastShownList.get(index);
+
+            if (!editPersonDescriptor.isAnyFieldEdited()) {
+                throw new CommandException(MESSAGE_NOT_EDITED);
+            }
+
+            Caregiver editedCaregiver = caregiverToEdit.edit(editPersonDescriptor);
+
+            if (!caregiverToEdit.isSamePerson(editedCaregiver) && model.hasPerson(editedCaregiver)) {
+                throw new CommandException("This caregiver already exists in the address book.");
+            }
+
+            model.setCaregiver(caregiverToEdit, editedCaregiver);
+            model.updateFilteredCaregiverList(Model.PREDICATE_SHOW_ALL_PERSONS);
+
+            return new CommandResult(
+                    String.format(MESSAGE_EDIT_PERSON_SUCCESS, "Caregiver", Messages.format(editedCaregiver)));
+        }
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this
+                || (other instanceof EditCommand)
+                && index == ((EditCommand) other).index
+                && editPersonDescriptor.equals(((EditCommand) other).editPersonDescriptor)
+                && isSenior == ((EditCommand) other).isSenior;
+    }
+}
