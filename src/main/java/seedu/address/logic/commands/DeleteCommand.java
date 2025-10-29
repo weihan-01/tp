26cf576e95c42d146d878cc1b660d23661d3cceb
whchenyicn <whchenyicn@gmail.com>
2025-10-30 00:31:41 +0800

    Update DeleteCommand error message

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -20,9 +20,9 @@ public class DeleteCommand extends Command {
     public static final String COMMAND_WORD = "delete";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Deletes the person identified by the index number used in the displayed person list.\n"
-            + "Parameters: INDEX (must be a positive integer)\n"
-            + "Example: " + COMMAND_WORD + " 1";
+            + ": Deletes the caregiver/senior identified by the index number used in the displayed caregiver/senior list.\n"
+            + "Parameters: s/INDEX or c/INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " c/1";
 
     public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
     public static final String MESSAGE_INVALID_SENIOR_INDEX = "No such senior index exists.";
